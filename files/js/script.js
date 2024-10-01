d353a365017183485e0106973812deb76a4bf551
Samuel <samueleonardo159@gmail.com>
2024-10-01 14:30:03 -0500

    Avance

--- files/js/script.js
+++ files/js/script.js
@@ -1,22 +1,20 @@
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
 inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+    input.addEventListener('focus', function() {
+        // Añade la clase 'focus' al contenedor padre cuando el input tiene foco
+        let parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+    });
+
+    input.addEventListener('blur', function() {
+        // Si el input está vacío al perder el foco, quita la clase 'focus'
+        if (this.value === "") {
+            let parent = this.parentNode.parentNode;
+            parent.classList.remove('focus');
+        }
+    });
 });
 
 
